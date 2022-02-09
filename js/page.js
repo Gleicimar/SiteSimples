d74d692f6a6291e0b5657360a969360284be2bb8
Gleicimar <gleicimarribeiro@gmail.com>
2022-02-09 00:16:24 -0300

    Portfolio
    Inicio      portfolio

---
+++ js/page.js
@@ -0,0 +1,7 @@
+const  btnMobile   = document.getElementById('button-mobile');
+
+function toggleMenu( ){
+const nav = document.getElementById('nav')
+nav.classList.toggle( 'active' );
+}
+btnMobile.addEventListener('click',toggleMenu);
